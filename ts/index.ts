85383252b3da62647f9809dea54e45326b750b5d
Philipp Kunz <kunzphp@gmail.com>
2015-11-30 06:55:55 +0100

    setup travis

--- ts/index.ts
+++ ts/index.ts
@@ -1,12 +1,16 @@
 /// <reference path="typings/tsd.d.ts" />
 var through = require("through2");
 var path = require("path");
+var beautylog = require("beautylog")("os");
 
-module.exports = (jadeTemplate,mojo = undefined) => {
+var pipelog:any = {} // module object
 
+pipelog.stream = (message:string,type:string,timing:string = "atEnd") => {
     return through.obj((file, enc, cb) => {
-        
+
         //run callback function to signal end of plugin process.
         return cb(null, file);
     });
 };
+
+module.exports = pipelog;
